0c59e22e6ee3b00472dc1a9a7c118ebea3b9eb76
garykhong <gary_khong@yahoo.com>
2017-05-24 17:45:40 +1000

    add form validation to the contact page

---
+++ TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheWorld.ViewModels;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public MailService MailService { get; private set; }
+        public IConfigurationRoot Config { get; private set; }
+
+        public AppController(MailService mailService, IConfigurationRoot config)
+        {
+            this.MailService = mailService;
+            this.Config = config;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if(model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("", "We don't support AOL addresses");
+            }
+            if(ModelState.IsValid)
+            {
+                MailService.SendMail(Config["MailSettings:ToAddress"], model.Email, "from the world", model.Message);
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
+            
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -7,22 +7,59 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld
 {
     public class Startup
     {
+        private IConfigurationRoot config;
+
+        public IHostingEnvironment Env { get; private set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            this.Env = env;
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables();
+
+            config = builder.Build();
+            
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(this.config);
+
+            if(Env.IsDevelopment())
+            {
+                services.AddScoped<MailService, DebugMailService>();
+            }
+            
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseDefaultFiles();
-            app.UseStaticFiles();            
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseStaticFiles();
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index"}
+                    );
+            });
         }
     }
 }
