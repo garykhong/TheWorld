ab0f1c59f78437f123470e1c8edbc7f934cd92a0
garykhong <gary_khong@yahoo.com>
2017-05-24 13:50:57 +1000

    with jquery to change the color of the background of the main element

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿(function () {    
+    var ele = $("#username");
+    ele.text("Gary Khong");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.css("background-color", "#888");
+    });
+
+    main.on("mouseleave", function () {
+        main.removeAttr("style")
+    });
+
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    });
+})();
