95a055e1e06e09479307d65e9f8c18b7b03863a1
garykhong <gary_khong@yahoo.com>
2017-05-29 11:09:01 +1000

    add angularjs contorller, validation and directive

--- TheWorld/wwwroot/js/app-trips.js
+++ TheWorld/wwwroot/js/app-trips.js
@@ -1,5 +1,5 @@
 ﻿(function () {
     "use strict"
     // creating the module
-    angular.module("app-trips", [])        
+    angular.module("app-trips", ["simpleControls"])        
 })();
--- TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/wwwroot/js/tripsController.js
@@ -5,16 +5,32 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         var vm = this;
-        vm.trips = [
-            {
-                name: "US Trip",
+        vm.trips = [];
+
+        vm.newTrip = {};
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                angular.copy(response.data, vm.trips);
+            }, function (error) {
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
+        vm.addTrip = function () {
+            vm.trips.push({
+                name: vm.newTrip.name,
                 created: new Date()
-            },
-            {
-                name: "World Trip",
-                created: new Date()
-            }];
+            });
+
+            vm.newTrip = {};
+        };
     }
 })();
